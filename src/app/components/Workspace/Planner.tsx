b79a200f3d1ccd3bb090636c76715c8dede07c29
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-03-21 18:52:26 +0530

    Updated planner list

--- src/app/components/Workspace/Planner.tsx
+++ src/app/components/Workspace/Planner.tsx
@@ -1,9 +1,53 @@
-import * as React from "react";
+"use client";
+
+import { useState } from "react";
 import Box from "@mui/material/Box";
-import { FixedSizeList } from "react-window";
-import PlannerListItem from "./PlannerListItem";
+import { FixedSizeList, ListChildComponentProps } from "react-window";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Checkbox from "@mui/material/Checkbox";
 
 export default function Planner() {
+  const [checked, setChecked] = useState([1]);
+
+  const handleToggle = (value: number) => () => {
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  function rowItem(props: ListChildComponentProps) {
+    const { index, style } = props;
+
+    const labelId = `checkbox-list-secondary-label-${index}`;
+
+    return (
+      <ListItem style={style} key={index} component="div" disablePadding>
+        <ListItemButton role={undefined} onClick={handleToggle(index)} dense>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={checked.indexOf(index) !== -1}
+              tabIndex={-1}
+              disableRipple
+              inputProps={{ "aria-labelledby": labelId }}
+            />
+          </ListItemIcon>
+          <ListItemText id={labelId} primary={`Item ${index + 1}`} />
+        </ListItemButton>
+      </ListItem>
+    );
+  }
+
   return (
     <Box
       sx={{
@@ -19,7 +63,7 @@ export default function Planner() {
         itemCount={200}
         overscanCount={5}
       >
-        {PlannerListItem}
+        {rowItem}
       </FixedSizeList>
     </Box>
   );

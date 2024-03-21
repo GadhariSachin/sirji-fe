939841654ed9ae42ff75b7bfb927d319db80ae0d
sachingadhari <37650415+GadhariSachin@users.noreply.github.com>
2024-03-21 17:37:59 +0530

    Added Planner List Item component

--- src/app/components/Workspace/Planner.tsx
+++ src/app/components/Workspace/Planner.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import { FixedSizeList } from "react-window";
+import PlannerListItem from "./PlannerListItem";
 
 export default function Planner() {
-  return <div>Planner</div>;
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        height: 800,
+        bgcolor: "background.paper",
+      }}
+    >
+      <FixedSizeList
+        height={780}
+        width={"100%"}
+        itemSize={46}
+        itemCount={200}
+        overscanCount={5}
+      >
+        {PlannerListItem}
+      </FixedSizeList>
+    </Box>
+  );
 }
